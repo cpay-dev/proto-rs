4bbdeda1196a5d643fd3a2636287bdf0df1f92d6
cpay.dev <203521523+cpay-dev-git@users.noreply.github.com>
2025-09-16 21:54:39 +0300

    upd src

--- crates/proto-build/src/main.rs
+++ crates/proto-build/src/main.rs
@@ -5,11 +5,10 @@ fn main() {
     .include_file(&"lib.rs")
     .compile_protos(
       &[
-        // blockchain/v1
-        "blockchain/v1/chain.proto",
         // api/v1
-        "api/v1/blockchain/chain.proto",
         "api/v1/blockchain/asset.proto",
+        "api/v1/blockchain/chain.proto",
+        "api/v1/blockchain/chain_id.proto",
         "api/v1/merchant/service.proto",
       ],
       &[&std::env::var("PROTO_SRC_DIR").expect("PROTO_SRC_DIR environment variable must be set")],
