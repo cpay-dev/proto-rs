f2f41a3d9898c6e5d1bc4034df027fdda4db3d4e
cpay.dev <203521523+cpay-dev-git@users.noreply.github.com>
2025-09-17 23:10:25 +0300

    build kms

--- crates/proto-build/src/main.rs
+++ crates/proto-build/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         "api/v1/blockchain/asset.proto",
         "api/v1/blockchain/chain.proto",
         "api/v1/blockchain/chain_id.proto",
+        "api/v1/kms/service.proto",
         "api/v1/merchant/service.proto",
       ],
       &[&std::env::var("PROTO_SRC_DIR").expect("PROTO_SRC_DIR environment variable must be set")],
