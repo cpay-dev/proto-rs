9e003c2ce6c616bb3b155353331745e61c02640b
cpay-dev-git <203521523+cpay-dev-git@users.noreply.github.com>
2025-09-20 23:01:55 +0300

    wallet service

--- crates/proto/src/lib.rs
+++ crates/proto/src/lib.rs
@@ -11,6 +11,9 @@ pub mod cpay {
             pub mod merchant {
                 include!("cpay.api.v1.merchant.rs");
             }
+            pub mod wallet {
+                include!("cpay.api.v1.wallet.rs");
+            }
         }
     }
 }
