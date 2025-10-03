b5343663be54e0a9403b76e65ff0cf3deecdf7c3
cpay.dev <203521523+cpay-dev-git@users.noreply.github.com>
2025-10-04 01:21:11 +0300

    refactor proto

--- crates/proto-build/src/main.rs
+++ crates/proto-build/src/main.rs
@@ -5,24 +5,29 @@ fn main() {
     .include_file(&"lib.rs")
     .compile_protos(
       &[
-        // api/v1
+        // api/v1/kms
         "api/v1/kms/service.proto",
 
-        "api/v1/merchant/asset.proto",
-        "api/v1/merchant/chain.proto",
-        "api/v1/merchant/payment-intent.proto",
-        "api/v1/merchant/asset-service.proto",
-        "api/v1/merchant/chain-service.proto",
-        "api/v1/merchant/payment-intent-service.proto",
-        
+        // api/v1/merchant
+        "api/v1/merchant/asset/asset.proto",
+        "api/v1/merchant/asset/service.proto",
+
+        "api/v1/merchant/chain/chain.proto",
+        "api/v1/merchant/chain/service.proto",
+
+        "api/v1/merchant/payment/intent.proto",
+        "api/v1/merchant/payment/service.proto",
+
+        // api/v1/wallet
         "api/v1/wallet/service.proto",
 
         // blockchain/v1
-        "blockchain/v1/indexer/block.proto",
-        "blockchain/v1/indexer/parsed-block.proto",
-        
         "blockchain/v1/block.proto",
         "blockchain/v1/chain.proto",
+
+        // blockchain/v1/indexer
+        "blockchain/v1/indexer/block.proto",
+        "blockchain/v1/indexer/parsed-block.proto",
       ],
       &[&std::env::var("PROTO_SRC_DIR").expect("PROTO_SRC_DIR environment variable must be set")],
     )
