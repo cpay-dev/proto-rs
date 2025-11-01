5ed0bffb5c6425b60e12a7a8d17f64ab70c5a170
EP <238863254+EPTF@users.noreply.github.com>
2025-11-01 14:03:52 +0300

    build user service

--- crates/proto-build/src/main.rs
+++ crates/proto-build/src/main.rs
@@ -9,6 +9,12 @@ fn main() {
 				"api/v1/authn/init_auth.proto",
 				"api/v1/authn/provider.proto",
 				"api/v1/authn/service.proto",
+				// api/v1/user
+				"api/v1/user/get_user_profile.proto",
+				"api/v1/user/identity.proto",
+				"api/v1/user/merchant.proto",
+				"api/v1/user/service.proto",
+				"api/v1/user/user.proto",
 			],
 			&[&std::env::var("PROTO_SRC_DIR").expect("PROTO_SRC_DIR environment variable must be set")],
 		)
